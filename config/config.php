826b4a39688751ebe508cf45c3024d4600216c80
Stretch Projects <StretchProjects@gmail.com>
2015-01-26 07:15:38 +0000

    Created Database library
    +Created Database library
    *Amended results/index to test Database library functionality (it was a
    copy of the league table template previously)

--- config/config.php
+++ config/config.php
@@ -4,3 +4,9 @@ const LIBRARY_FOLDER = 'library/';
 const CONTROLLER_FOLDER = 'Controller/';
 const MODEL_FOLDER = 'model/';
 const VIEW_FOLDER = 'view/';
+
+const DATABASE_TYPE = 'mysql';
+const DATABASE_HOST = 'workspace';
+const DATABASE_NAME = 'league';
+const DATABASE_USER = 'root';
+const DATABASE_PASS = 'mypassword';
---
+++ library/Database.php
@@ -0,0 +1,74 @@
+<?php
+
+class Database extends PDO {
+
+    public function __construct() {
+        parent::__construct(DATABASE_TYPE . ':host=' . DATABASE_HOST . ';dbname=' . DATABASE_NAME, DATABASE_USER, DATABASE_PASS);
+    }
+
+    public function select($sql, $values = array(), $fetchMode = PDO::FETCH_ASSOC) {
+        $query = $this->prepare($sql);
+
+        foreach ($values as $key => $value) {
+            $query->bindValue($key, $value);
+        }
+
+        if (!$query->execute()) {
+            require_once ControllerFolder . 'error.php';
+            $e = new error();
+            $e->index();
+            return;
+        }
+
+        return $query->fetchAll($fetchMode);
+    }
+
+    public function insert($table, $data) {
+        $keys = implode('`,`', array_keys($data)); // 'val1`, `val2'
+        $values = ':' . implode(', :', array_keys($data)); // ':val1, :val2';
+
+        $query = $this->prepare("INSERT INTO $table ( `$keys` ) VALUES ( $values );");
+
+        foreach ($data as $key => $value) {
+            $query->bindValue($key, $value);
+        }
+
+        if (!$query->execute()) {
+            require_once ControllerFolder . 'error.php';
+            $e = new error();
+            $e->index();
+            return;
+        }
+        return $this->lastInsertId();
+    }
+
+    public function update($table, $data, $where) {
+        ksort($data);
+
+        $datastring = '';
+        foreach ($data as $key => $value) {
+            $datastring .= "`$key`=:$key, ";
+        }
+        $datastringcleaned = rtrim($datastring, ', ');
+
+        $query = $this->prepare("UPDATE $table SET $datastringcleaned WHERE $where;");
+
+        foreach ($data as $key => $value) {
+            $query->bindValue($key, $value);
+        }
+
+        if (!$query->execute()) {
+            require_once ControllerFolder . 'error.php';
+            $e = new error();
+            $e->index();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function delete($table, $where, $limit = 1) {
+        return $this->exec("DELETE FROM $table WHERE $where LIMIT $limit;");
+    }
+
+}
